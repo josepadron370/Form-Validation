ea5e305ba93e770180b20e76ec3d5740642db641
josepadron370 <josepadron370@gmail.com>
2019-12-05 21:13:39 +0000

    second commit

--- index.js
+++ index.js
@@ -12,54 +12,54 @@ function checkForm(e) {
     let cc = document.querySelector('#cc');
 
     if (cc.value == "") {
-        error_list +=  "<div>Credit Card field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>Credit Card field can't be empty.</div>";
     }
 
 
     let cvc = document.querySelector('#cvc');
 
     if (cvc.value == "") {
-        error_list +=  "<div>CVC field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>CVC field can't be empty.</div>";
     }
 
     let amount = document.querySelector('#amount');
 
     if (amount.value == "") {
-        error_list +=  "<div>Amount field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>Amount field can't be empty.</div>";
     }
 
     let first = document.querySelector('#first');
 
     if (first.value == "") {
-        error_list +=  "<div>First Name field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>First Name field can't be empty.</div>";
     }
 
     let last = document.querySelector('#last');
 
     if (last.value == "") {
-        error_list +=  "<div>Last Name field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>Last Name field can't be empty.</div>";
     }
 
 
     if (document.querySelector('#city').value == "") {
-        error_list +=  "<div>City field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>City field can't be empty.</div>";
     }
 
     let state = document.querySelector('#state');
 
     if (state.value == "Pick a state") {
-        error_list +=  "<div>State field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>State field can't be empty.</div>";
     }
 
     let zip = document.querySelector('#zip');
 
     if (zip.value == "") {
-        error_list +=  "<div>Zip field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>Zip field can't be empty.</div>";
     }
 
 
     if (document.querySelector('#message').value == "") {
-        error_list +=  "<div>Message field can't be empty.</div>";
+        error_list +=  "<div class='alert alert-danger'>Message field can't be empty.</div>";
     }
 
 
@@ -74,7 +74,7 @@ function checkForm(e) {
 
 
     if (!cc1.checked && !cc2.checked && !cc3.checked && !cc4.checked) {
-        error_list +=  "<div>One must be checked.</div>";
+        error_list +=  "<div class='alert alert-danger'>One must be checked.</div>";
     }
 
 
